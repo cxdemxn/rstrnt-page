df6bf3f0eb390ca9c0a9dff35de8e575ce5bdfaa
barizaa <donbarizaa@gmail.com>
2024-07-16 02:20:18 +0100

    hamburger menu fully functional now

--- src/index.js
+++ src/index.js
@@ -23,8 +23,24 @@ const galleryNavBar = document.querySelector('.galleryNavBtn');
 const contactNavBtn = document.querySelector('.contactNavBtn');
 const orderNavBar = document.querySelector('.orderNavBtn');
 const accountNavBtn = document.querySelector('.accountNavBtn');
+const menuBtn = document.querySelector('.menu-button');
 // const closeOverlay = document.querySelector('.closeOverlay');
 
+menuBtn.addEventListener('click', () => {
+    console.log('menu button was clicked');
+    const overlay = document.querySelector('.overlay')
+    if (overlay.style.width === '100%') { // closing the menu overlay
+        overlay.style.width = '0';
+        document.querySelector('#menuOpen').style.display = 'block';
+        document.querySelector('#menuClose').style.display = 'none';
+        document.body.classList.remove('no-scroll');
+    } else {
+        overlay.style.width = '100%';
+        document.querySelector('#menuOpen').style.display = 'none';
+        document.querySelector('#menuClose').style.display = 'block';
+        document.body.classList.add('no-scroll');
+    }
+})
 
 // closeOverlay.addEventListener('click', () => {
 //     console.log('close overlay was clicked');
